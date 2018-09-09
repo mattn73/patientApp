b961a52e3806434ff3b4c110cb92488d9682802e
mattn73 <MattyConhyea@gmail.com>
2018-09-09 22:12:19 +0400

    setup project

---
+++ application/views/templates/footer.php
@@ -0,0 +1,7 @@
+
+
+
+<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script
+</body>
+</html>
---
+++ application/views/templates/header.php
@@ -0,0 +1,9 @@
+<html lang="en">
+<head>
+	<meta charset="utf-8">
+	<title>APP</title>
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+
+</head>
+<body>
+
