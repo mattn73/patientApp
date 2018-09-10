4afd97e3d0079cd50662cf50ef1b30cb035dedc8
mattn73 <MattyConhyea@gmail.com>
2018-09-10 23:52:02 +0400

    add side panel with swipe effect open

--- application/views/templates/footer.php
+++ application/views/templates/footer.php
@@ -1,7 +1,52 @@
 
 
+  </div>
+        </div>
 
-<script type="text/javascript" src="<?php echo base_url("assets/js/jQuery.js"); ?>"></script>
-<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script
+<div class="overlay"></div>
+
+
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/popper.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/bootstrap.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery.mCustomScrollbar.concat.min.js"); ?>"></script>
+<script type="text/javascript" src="<?php echo base_url("assets/js/jquery.mobile.custom.min.js"); ?>"></script>
+
+
+    <script type="text/javascript">
+        $(document).ready(function () {
+            $("#sidebar").mCustomScrollbar({
+                theme: "minimal"
+            });
+
+            $('#dismiss, .overlay').on('click', function () {
+                $('#sidebar').removeClass('active');
+                $('.overlay').removeClass('active');
+            });
+
+            $('#sidebarCollapse').on('click', function () {
+                $('#sidebar').addClass('active');
+                $('.overlay').addClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+            });
+        });
+
+     $( document ).ready(function() {
+         $( document ).on( "swipeleft swiperight", function( e ) {
+        
+        if (!($( ".overlay" ).hasClass('active'))) {
+            if ( e.type === "swiperight" ) {
+                $('#sidebar').addClass('active');
+                $('.overlay').addClass('active');
+                $('.collapse.in').toggleClass('in');
+                $('a[aria-expanded=true]').attr('aria-expanded', 'false');
+           
+            }
+        }
+    });
+});
+    </script>
+
 </body>
 </html>
--- application/views/templates/header.php
+++ application/views/templates/header.php
@@ -1,9 +1,86 @@
 <html lang="en">
 <head>
-	<meta charset="utf-8">
+
 	<title>APP</title>
 	<link rel="stylesheet" href="<?php echo base_url("assets/css/bootstrap.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/style.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/jquery.mCustomScrollbar.min.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/jquery.mobile.custom.theme.min.css"); ?>" />
+	<link rel="stylesheet" href="<?php echo base_url("assets/css/jquery.mobile.custom.structure.min.css"); ?>" />
+
+	<script defer src="<?php echo base_url("assets/js/solid.min.js"); ?>"></script>
+    <script defer src="<?php echo base_url("assets/js/fontawesome.min.js"); ?>"></script>
+
+	<meta charset="utf-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
 
 </head>
 <body>
 
+<div class="wrapper">
+        <!-- Sidebar  -->
+        <nav id="sidebar">
+            <div id="dismiss">
+                <i class="fas fa-arrow-left"></i>
+            </div>
+
+            <div class="sidebar-header">
+                <h3>Bootstrap Sidebar</h3>
+            </div>
+
+            <ul class="list-unstyled components">
+                <div class='side-header'>
+                <p>Dummy Heading</p>
+
+                </div>    
+            
+                <li class="active">
+                    <a href="#homeSubmenu" data-toggle="collapse" aria-expanded="false">Home</a>
+                    <ul class="collapse list-unstyled" id="homeSubmenu">
+                        <li>
+                            <a href="#">Home 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Home 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">About</a>
+                    <a href="#pageSubmenu" data-toggle="collapse" aria-expanded="false">Pages</a>
+                    <ul class="collapse list-unstyled" id="pageSubmenu">
+                        <li>
+                            <a href="#">Page 1</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 2</a>
+                        </li>
+                        <li>
+                            <a href="#">Page 3</a>
+                        </li>
+                    </ul>
+                </li>
+                <li>
+                    <a href="#">Portfolio</a>
+                </li>
+                <li>
+                    <a href="#">Contact</a>
+                </li>
+            </ul>
+
+            
+        </nav>
+
+  <div id="content">
+                <button type="button" id="sidebarCollapse" class="btn ">
+                    <!-- <i class="fas fa-align-left"></i> -->
+                    <div class="hamburger">
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                        <span class="bar"></span>
+                    </div>
+                </button> 
