9d012b50eb42e05fe1868f6a1e711de48ca98f4f
mattn73 <MattyConhyea@gmail.com>
2018-09-29 23:34:45 +0400

    add firebase

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,5 +1,17 @@
 
 
-  $('.hamburger').click(function(){
-    $(this).toggleClass('responsive');
-  })
+$('.hamburger').click(function () {
+  $(this).toggleClass('responsive');
+});
+
+
+
+$(document).ready(function () {
+  var link = document.createElement("link");
+  link.href = "assets/css/style.css";
+  link.type = "text/css";
+  link.rel = "stylesheet";
+  document.getElementsByTagName("head")[0].appendChild(link);
+
+});
+
