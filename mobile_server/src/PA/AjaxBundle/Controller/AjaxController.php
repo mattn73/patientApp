981060aaad9411c49ea39ddb0d7bb6deb978c52e
matthieu conhyea <mattyconhyea@gmail.com>
2018-10-08 19:13:52 +0400

    create entity user

--- mobile_server/src/PA/AjaxBundle/Controller/AjaxController.php
+++ mobile_server/src/PA/AjaxBundle/Controller/AjaxController.php
@@ -2,7 +2,12 @@
 
 namespace PA\AjaxBundle\Controller;
 
+
 use Symfony\Bundle\FrameworkBundle\Controller\Controller;
+use Symfony\Component\HttpFoundation\JsonResponse;
+use Symfony\Component\HttpFoundation\Request;
+use Symfony\Component\HttpFoundation\Response;
+use Symfony\Component\HttpFoundation\Session\Session;
 
 class AjaxController extends Controller
 {
@@ -10,4 +15,34 @@ class AjaxController extends Controller
     {
         return $this->render('PAAjaxBundle:Default:index.html.twig');
     }
+
+
+    /**
+     * @param Request $request
+     *
+     * @return Response
+     */
+    public function registerAction(Request $request)
+    {
+        $username = $request->get('uname');
+        $email = $request->get('email');
+        $password = $request->get('psw');
+        $age = $request->get('age');
+        $sex = $request->get('sex');
+        $em = $this->getDoctrine()->getManager();
+
+
+        $country = $em->getRepository(Country::class)->findOneBy(['countryCode' => $countryCode]);
+
+        /** @var RegionRepository $repo */
+        $repo = $em->getRepository(Region::class);
+        $regionList = $repo->getRegionByCountryId($country->getId());
+
+        return $this->render('AppBundle:Ajax:get_region.html.twig', [
+            'regionList' => $regionList,
+        ]);
+    }
 }
+
+
+
